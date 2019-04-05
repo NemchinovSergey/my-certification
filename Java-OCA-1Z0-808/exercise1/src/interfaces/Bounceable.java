b126f60d90153baa6ec506c8af253301c9811d3f
Nemchinov Sergey <nemchinovsv@novotelecom.ru>
2019-04-05 11:08:55 +0700

    interfaces declaration

---
+++ Java-OCA-1Z0-808/exercise1/src/interfaces/Bounceable.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+// modifier 'abstract' is redundant for interfaces
+public abstract interface Bounceable {
+
+    // these are the same and they are public abstract void bounce();
+    void bounce();
+//    public void bounce();
+//    abstract void bounce();
+//    abstract public void bounce();
+//    public abstract void bounce();
+
+}
