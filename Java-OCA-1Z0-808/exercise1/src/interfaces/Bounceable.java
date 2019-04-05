e3225dda5ed1c7813642ee9b7b733185316f4d0f
Nemchinov Sergey <nemchinovsv@novotelecom.ru>
2019-04-05 11:16:11 +0700

    illegal interfaces declaration

--- Java-OCA-1Z0-808/exercise1/src/interfaces/Bounceable.java
+++ Java-OCA-1Z0-808/exercise1/src/interfaces/Bounceable.java
@@ -1,13 +1,30 @@
 package interfaces;
 
-// modifier 'abstract' is redundant for interfaces
+/*
+   modifier 'abstract' is redundant for interfaces
+ */
 public abstract interface Bounceable {
 
-    // these are the same and they are public abstract void bounce();
+    /*
+     * these are the same and they are public abstract void bounce();
+     */
     void bounce();
 //    public void bounce();
 //    abstract void bounce();
 //    abstract public void bounce();
 //    public abstract void bounce();
+
+
+    /*
+     * final and abstract can never be used together,
+     * and abstract is implied
+     */
+//    final void theImpossibleMethod();
+
+    /*
+     * interface methods are always public
+     */
+//    private void theImpossibleMethod();
+//    protected void theImpossibleMethod();
 
 }
