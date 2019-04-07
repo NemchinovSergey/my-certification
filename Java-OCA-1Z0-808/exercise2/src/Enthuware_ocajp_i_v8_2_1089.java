b10ee50572ed5ff8934886cf3fb6497f99bcefe6
Nemchinov Sergey <googoodemon@gmail.com>
2019-04-07 20:02:22 +0700

    renamed into Enthuware_ocajp_i_v8_2_1089

--- Java-OCA-1Z0-808/exercise2/src/Enthuware_ocajp_i_v8_2_1089.java
+++ Java-OCA-1Z0-808/exercise2/src/Enthuware_ocajp_i_v8_2_1089.java
@@ -1,9 +1,9 @@
-public class DragAndDropTestClass1 {
+/**
+ * Question enthuware.ocajp.i.v8.2.1089
+ * @see <a href="https://enthuware.com/forum/viewtopic.php?f=2&t=1530">About Question enthuware.ocajp.i.v7.2.1089</a>
+ */
+public class Enthuware_ocajp_i_v8_2_1089 {
 
-    /**
-     * Question enthuware.ocajp.i.v8.2.1089
-     * @see <a href="https://enthuware.com/forum/viewtopic.php?f=2&t=1530">About Question enthuware.ocajp.i.v7.2.1089</a>
-     */
     public static void main(String args[]) {
         Short k = 9; Integer i = 9; Boolean b = false;
         char c = 'a'; String str = "123";
